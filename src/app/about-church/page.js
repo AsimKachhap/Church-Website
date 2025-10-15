28ec97ba41f32e2e4c55fa30b934e037448fdf92
unknown <asim26k@gmail.com>
2025-10-16 00:21:29 +0530

    Add about-church page

---
+++ src/app/about-church/page.js
@@ -0,0 +1,73 @@
+import React from "react";
+
+const AboutChurch = () => {
+  return (
+    <div className="  flex flex-col justify-center items-center px-64 pt-8 ">
+      <h1 className="text-blue-700 text-4xl font-bold">
+        The History of St. Mary's Assumption Church
+      </h1>
+      <h2 className="text-amber-500 text-2xl uppercase font-medium">
+        Mariam Nagar Ghaziabad
+      </h2>
+      <p>
+        The seeds of divine vision were sown in 1982, whe Sanjo CMI Bhavan was
+        established at Patel Nagar, Ghaziabad. The Mariam Nagar Complex, as we
+        know today, was born from the sacred vision of Rev. Fr. Gerald
+        Pandinjarepeedika CMI. Guided by God's providence, this dremn began to
+        take form in 1989 when a new campus was established and blessed in
+        Mariam Nagar.
+      </p>
+      <p>
+        Years earlier in 1979, Fr. Amadeus OFM had founded a school under the
+        management of Sacred Heart Church, Patel Nagar. Responding to divine
+        call, the CMI Fathers were invited to take over the school, which was
+        renamed St. Joseph's Academy in the year 1982-83. The St. Joseph's
+        Academy was relocated to Mariam Nagar on July 16, 1989, marking a new
+        chapter in the mission of Catholic educationin the region.
+      </p>
+      <p>
+        In the fullness of God's time, St. Mary's Assumption Church was
+        officially erected as Parish on 15th August 2007, a date filled with
+        Marian significance. The declaration was made by His Excellency Bishop
+        Patric Nair of the Meerut Diocese, and the pastoral care of the Parish
+        was entrusted to the Carmelities of Mary Immaculate, St. John's
+        Provience, Bijnor.
+      </p>
+      <p>
+        It remains the previlae of the Provincial to recommend the name of the
+        Parish Priest, whom the Bishop then appoints with the necessary
+        ecclesiastical faculties. THe CMI Fathers, with their charism of
+        service, minister to all Catholics in the area, irrespective of their
+        rite - be it Latin or Syro-Malabar - celebrating the liturgy in ways
+        meaningful to each community.
+      </p>
+      <p>
+        The land of the church are under the jurisdiction of the Meerut Diocese,
+        ensuring the spiritual nourishment is deeply rooted in diocesan
+        tradition and guidance.{" "}
+      </p>
+      <p>
+        At Present we have 200 registered families. It is a Vibrant mosiac of
+        cultures and communities - Adivasis from Chota Nagpur, Bengalis,
+        Malyalis, and North Indians - all united in Christ. With six wards , a
+        proactive Parish Council, and multiple pious groups, the church pulses
+        with life and mission.
+      </p>
+      <p>
+        Mariam Nagar is graced with seven religious congregations, including the
+        CMI Fathers, each playing a significant role in nurturing faith. On
+        Sundays, Rev. Fathers and Sisters gggo forth to visit Catholic homes,
+        lead prayer meetings, and strengthen the small Christian Communities in
+        each ward.
+      </p>
+      <p>
+        Events such as home mission, one-day retreats, and youth camps are
+        regularly held to help the people grow in the spirit of faith.
+        Cathechism is diligently conducted, aligned with the Meerut Diocese
+        syllabus, ensuring that our children row in faith and understanding.
+      </p>
+    </div>
+  );
+};
+
+export default AboutChurch;
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,43 @@
 import Navbar from "@/components/Navbar";
+import Image from "next/image";
+import churchImage from "../assets/Church.jpg";
 
 export default function Home() {
   return (
     <div className=" bg-white">
       <div className="">
         <Navbar />
+        <div className="hero-section-mobile md:hidden">
+          <Image
+            src={churchImage}
+            alt="Picture of St. Mary's Assumption Church"
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        <div className="hero-section hidden h-screen md:display md:flex justify-center items-center ">
+          <div className="relative w-1/2 h-full bg-red-500">
+            <Image
+              src={churchImage}
+              alt="Picture of St. Mary's Assumption Church"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className=" w-1/2 flex flex-col items-center justify-center p-8">
+            <h2>About the Church</h2>
+            <p>
+              The Seeds of divine vision were sown in 1982, when Sanjo CMI
+              Bhawan was established at Patel Nagar, Ghaziabad. The Mariam Nagar
+              Complex, as we know it today, was born from the sacred vision of
+              Rev. Fr. Gerald Padinjarepeedika CMI. Guided by God's providence,
+              this dream began to take form in 1989 when a new campus was
+              established in Mariam Nagar.
+            </p>
+            <p>ear</p>
+          </div>
+        </div>
       </div>
     </div>
   );
