9cc80b630ee7a8b5b6ddff45d10cb3bf7a2a3fc7
unknown <asim26k@gmail.com>
2025-11-04 10:56:45 +0530

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,14 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div className=" md:hidden sticky z-50  top-0 flex justify-between items-center  bg-indigo-200/70 border border-indigo-700 backdrop-blur-md px-4 p-2 text-indigo-700 ">
-        <div className=" font-display ">St. Mary's Assumption Church</div>
+      <div className=" md:hidden sticky z-50  top-0 flex justify-between items-center  bg-primary  backdrop-blur-md px-4 py-4 text-white ">
+        <div className=" font-display text-displayColor ">
+          St. Mary's Assumption Church
+        </div>
         <div>
           <a href="#how-to-reach">
-            <button className="p-[3px] relative cursor-pointer ">
-              <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-              <div className="px-8 py-2  uppercase bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+            <button className=" relative cursor-pointer ">
+              <div className="px-4 py-2  uppercase bg-accent   relative group transition duration-200 text-text-primary active:scale-95 active:translate-y-1 ">
                 How to Reach
               </div>
             </button>
@@ -17,13 +18,13 @@ const Navbar = () => {
         </div>
       </div>
       <div className="hidden md:display sticky z-50 top-0 md:flex justify-between items-center  text-text-primary bg-primary border  backdrop-blur-md px-8 p-4">
-        <div className="font-display text-displayColor">
+        <div className="font-display text-displayColor text-2xl">
           St. Mary's Assumption Church
         </div>
         <div>
           <a href="#how-to-reach">
             <button className="p-[3px] relative cursor-pointer ">
-              <div className="px-8 py-2  uppercase bg-accent  relative group transition duration-200 text-white hover:bg-accent shadow-md  hover:scale-105 ">
+              <div className="px-8 py-2  uppercase bg-accent  relative group transition duration-200 text-text-primary hover:bg-accent shadow-md  hover:scale-105 hover:-translate-y-1  ">
                 How to Reach
               </div>
             </button>
