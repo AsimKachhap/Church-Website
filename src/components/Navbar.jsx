e7db803b0373a398054fe682f561c7f0f6deaadd
unknown <asim26k@gmail.com>
2025-11-04 11:14:17 +0530

    Adjust padding on 'How to Reach' Button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
         <div>
           <a href="#how-to-reach">
             <button className=" relative cursor-pointer ">
-              <div className="px-2 py-1  uppercase text-[10px] bg-accent   relative group transition duration-200 text-text-primary active:scale-95 active:translate-y-1 ">
+              <div className="px-2 py-[6px]  uppercase text-[10px] bg-accent   relative group transition duration-200 text-text-primary active:scale-95 active:translate-y-1 ">
                 How to Reach
               </div>
             </button>
@@ -24,7 +24,7 @@ const Navbar = () => {
         <div>
           <a href="#how-to-reach">
             <button className="p-[3px] relative cursor-pointer ">
-              <div className="px-8 py-2  uppercase bg-accent  relative group transition duration-200 text-text-primary hover:bg-accent shadow-md  hover:scale-105 hover:-translate-y-1  ">
+              <div className="px-4 py-2  uppercase bg-accent  relative group transition duration-200 text-text-primary hover:bg-accent shadow-md  hover:scale-105 hover:-translate-y-1  ">
                 How to Reach
               </div>
             </button>
